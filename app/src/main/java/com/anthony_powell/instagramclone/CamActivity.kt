bb494f22a97d8de22cad56164b3ba1e540047c1f
antpowell <powell.anthony.e@gmail.com>
2018-09-15 19:53:20 -0500

    Initial commit

---
+++ app/src/main/java/com/anthony_powell/instagramclone/CamActivity.kt
@@ -0,0 +1,12 @@
+package com.anthony_powell.instagramclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cam)
+    }
+}
